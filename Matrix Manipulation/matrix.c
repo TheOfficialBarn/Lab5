e6c70bbe61ec45c694734ebb2dd7b4e6a1ae7a8a
Aiden Dorantes Barnard <apbarnard2004@icloud.com>
2024-10-13 16:44:51 -0500

    Finished matrix

--- Matrix Manipulation/matrix.c
+++ Matrix Manipulation/matrix.c
@@ -3,10 +3,15 @@
 #include <stdlib.h> //
 #include <stdbool.h> //For true/false
 
+#define SIZE 5
+
+
+int getMatrices(int oneOrTwo);
+
 void addTwoMatrices(); 
 void multiplyTwoMatrices();
 void transposeMatrix();
-void printMatrix();
+void printMatrix(int m[SIZE][SIZE]);
 
 int main() {
 	int userInput;
@@ -14,7 +19,7 @@ int main() {
 	//Ask which function the user would like to run
 	while(true) {
 		printf("Which program would you like to run?\n");
-		printf("1) Add Two Matrices\n2) Multiply Two Matrices\n3) Transpose a Matrix\n4) Print a Matrix\n5) Quit\nSelect a function (1-5): ");
+		printf("1) Add Two Matrices\n2) Multiply Two Matrices\n3) Transpose a Matrix\n4) Quit\nSelect a function (1-4): ");
 		scanf("%d",&userInput);
 		printf("\n");
 
@@ -24,28 +29,110 @@ int main() {
 				addTwoMatrices();
 				break;
 			case 2:
-				printf("Running Function 2...\n--Multiply Two Matrices--");
+				printf("Running Function 2...\n--Multiply Two Matrices--\n");
 				multiplyTwoMatrices();
 				break;
 			case 3:
-				printf("Running Function 3...\n--Transpose a Matrix--");
+				printf("Running Function 3...\n--Transpose a Matrix--\n");
 				transposeMatrix();
 				break;
 			case 4:
-				printf("Running Function 4...\n--Print a Matrix--");
-				printMatrix();
-				break;
-			case 5:
 				printf("Quitting Program...");
 				break;
 			default:
-				printf("Select a valid input between 1 and 5.");
+				printf("Select a valid input between 1 and 4.");
 		}
 
-		if(userInput == 5) {
+		if(userInput == 4) {
 			break;
 		}
-
-
 	}
+}
+
+void addTwoMatrices() {
+	int m1[SIZE][SIZE] = {
+		{1, 2, 3, 4, 5},
+		{6, 7, 8, 9, 10},
+		{11, 12, 13, 14, 15},
+		{16, 17, 18, 19, 20},
+		{21, 22, 23, 24, 25}
+	};
+
+	int m2[SIZE][SIZE] = {
+		{25, 24, 23, 22, 21},
+		{20, 19, 18, 17, 16},
+		{15, 14, 13, 12, 11},
+		{10, 9, 8, 7, 6},
+		{5, 4, 3, 2, 1}
+	};
+
+	int m3[SIZE][SIZE];
+	for(int x=0; x<SIZE; x++){
+		for(int y=0; y<SIZE; y++){
+			//Perform matrix addition 
+			m3[x][y] = m1[x][y]+m2[x][y];
+
+		}
+	}
+	printMatrix(m3);
+
+	//Return m3
+}
+
+void multiplyTwoMatrices() {
+	int m1[SIZE][SIZE] = {
+		{1, 2, 3, 4, 5},
+		{6, 7, 8, 9, 10},
+		{11, 12, 13, 14, 15},
+		{16, 17, 18, 19, 20},
+		{21, 22, 23, 24, 25}
+	};
+
+	int m2[SIZE][SIZE] = {
+		{25, 24, 23, 22, 21},
+		{20, 19, 18, 17, 16},
+		{15, 14, 13, 12, 11},
+		{10, 9, 8, 7, 6},
+		{5, 4, 3, 2, 1}
+	};
+	int m3[SIZE][SIZE] = {0}; //We have to initialize the matrix with zeroes due to the use of += operator
+	for(int x=0; x<SIZE; x++){
+		for(int y=0; y<SIZE; y++){
+			for(int z = 0; z < SIZE; z++) {
+            m3[x][y] += m1[x][z] * m2[z][y];
+			}
+		}
+	}
+	printMatrix(m3);
+	//Return m3
+}
+
+void transposeMatrix() {
+	int m1[SIZE][SIZE] = {
+		{1, 2, 3, 4, 5},
+		{6, 7, 8, 9, 10},
+		{11, 12, 13, 14, 15},
+		{16, 17, 18, 19, 20},
+		{21, 22, 23, 24, 25}
+	};
+
+	int mT[SIZE][SIZE];
+
+	for(int x=0; x<SIZE; x++){
+		for(int y=0; y<SIZE; y++){
+			mT[y][x] = m1[x][y];
+		}
+	}
+	printMatrix(mT);
+	//Return mT
+}
+
+void printMatrix(int m[SIZE][SIZE]) {
+	for(int x=0; x<SIZE; x++){
+		for(int y=0; y<SIZE; y++){
+			printf("%6d", m[x][y]);
+		}
+		printf("\n");
+	}
+	printf("\n———————————————————————\n\n");
 }
